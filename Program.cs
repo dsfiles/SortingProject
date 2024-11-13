30bb1ddeb7c717d2d572966e5287531a9e7eede8
gli <gli@SMU13041>
2024-11-13 08:28:15 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SortingProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sorting project");
+        }
+    }
+}
