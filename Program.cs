d6c51ca8d25ab1228cbf9dc8f5a85e13a0394208
gli <gli@SMU13041>
2024-11-13 09:16:03 -0800

    3rd update

--- Program.cs
+++ Program.cs
@@ -9,64 +9,12 @@
             int[] a = { 1, 3, 5, 7 };
             int[] b = { 2, 4, 6, 8, 10, 20 };
             int[] c = new int[a.Length + b.Length];
-            Merge(a, b, c);
+            Sort.Merge(a, b, c);
             Console.WriteLine("after merging:");
             Print(c);
         }
 
-        static void Merge(int[] a, int[] b, int[] c) {
-            int i = 0, j = 0, k = 0; // index i for a, j for b, k for c
-
-            /* your code
-            while( both a and b are not empty ){
-                read the smaller number and store it in array c
-            }*/
-            while (i < a.Length && j < b.Length) {
-                if (a[i] < b[j])
-                {
-                    c[k] = a[i];
-                    i++;
-                    k++;
-                }
-                else
-                {
-                    c[k] = b[j];
-                    j++; 
-                    k++;
-                }
-            }
-
-
-
-            /*
-            while( a is not empty){
-                read all the numbes from a into c
-            }
-            */
-            while (i < a.Length)
-            {
-                c[k] = a[i];
-                i++;
-                k++;
-            }
-
-
-            /*
-            while( b is not empty){
-                read all the numbes from b into c
-            }
-            */
-            while (j < b.Length)
-            {
-                c[k] = b[j];
-                j++;
-                k++;
-            }
-
-        }
-
-
-        static void Print(int[] arr)
+         static void Print(int[] arr)
         {
             foreach (int i in arr)
             {
---
+++ Sort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingProject
+{
+    public class Sort
+    {
+        public static void Merge(int[] a, int[] b, int[] c)
+        {
+            int i = 0, j = 0, k = 0; // index i for a, j for b, k for c
+            /* 
+            while( both a and b are not empty ){
+                read the smaller number and store it in array c
+            }*/
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    c[k++] = a[i++];
+                }
+                else
+                {
+                    c[k++] = b[j++];
+                }
+            }
+            /*
+            while( a is not empty){
+                read all the numbes from a into c
+            } */
+            while (i < a.Length)
+            {
+                c[k++] = a[i++];
+            }
+            /*
+            while( b is not empty){
+                read all the numbes from b into c
+            } */
+            while (j < b.Length)
+            {
+                c[k++] = b[j++];
+            }
+        }
+    }
+}
